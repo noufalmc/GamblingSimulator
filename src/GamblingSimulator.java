fb0c064f23e4e267fd2df98a7b0031a0b4d24813
noufalmc123 <47408615+noufalmc123@users.noreply.github.com>
2021-12-29 09:41:30 +0530

    Added:WelcomeMessage

---
+++ src/GamblingSimulator.java
@@ -0,0 +1,10 @@
+/*
+Author:Noufal MC
+Date:29/12/2021
+* */
+public class GamblingSimulator {
+    public static void main(String[] args)
+    {
+        System.out.println("Welcom Gambling Simulator");
+    }
+}
