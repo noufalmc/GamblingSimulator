5035896513d1d506e4b45d789189ed1bd354eaeb
noufalmc123 <47408615+noufalmc123@users.noreply.github.com>
2021-12-29 09:44:07 +0530

    Added:Start and Bet value added

--- src/GamblingSimulator.java
+++ src/GamblingSimulator.java
@@ -3,6 +3,9 @@ Author:Noufal MC
 Date:29/12/2021
 * */
 public class GamblingSimulator {
+    //Initialize the value
+    static  int START=100;
+    static final int BET=1;
     public static void main(String[] args)
     {
         System.out.println("Welcom Gambling Simulator");
