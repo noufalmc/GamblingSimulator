cd9e7ddcc90027db3e2bc485c3077590391f3c2a
noufalmc123 <47408615+noufalmc123@users.noreply.github.com>
2021-12-29 09:50:04 +0530

    Added:Win or loss added

--- src/GamblingSimulator.java
+++ src/GamblingSimulator.java
@@ -2,12 +2,27 @@
 Author:Noufal MC
 Date:29/12/2021
 * */
+import java.util.Random;
 public class GamblingSimulator {
     //Initialize the value
     static  int START=100;
     static final int BET=1;
+    public static void WinorLoss()
+    {
+      Random ra=new Random();
+      int VALUE=ra.nextInt(2    );
+      if(VALUE==1)
+      {
+          System.out.println("Won the Game");
+      }
+      else
+      {
+          System.out.println("Loss the Game");
+      }
+    }
     public static void main(String[] args)
     {
         System.out.println("Welcom Gambling Simulator");
+        WinorLoss();
     }
 }
